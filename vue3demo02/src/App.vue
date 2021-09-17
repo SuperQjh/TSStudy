50abe83eea1723dafcd7ae9b334afd08407cbb46
312173544@qq.com <312173544@qq.com>
2021-09-17 17:29:01 +0800

    no message

---
+++ vue3demo02/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div @click="addCount">{{count}}</div>
+  <div>{{`姓名${per.name}, 年纪${per.age}, 性别${per.sex}`}}</div>
+  <student/>
+</template>
+
+<script>
+import {ref, reactive, provide} from 'vue';
+import Student from './components/Student.vue'
+
+export default {
+  components: {
+    Student
+  },
+  setup() {
+  const count = ref(1)
+  function addCount() {
+  count.value++
+}
+const per = reactive({
+  name: '张三',
+  age: 23,
+  sex: '男'
+})
+const student = reactive({
+  name: '小米',
+  classname: '三年级一班'
+})
+provide('student', student)
+  return {per}
+}
+}
+
+
+</script>
---
+++ vue3demo02/src/components/Student.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>学生</div>
+  <div>姓名：{{name}}</div>
+  <div>班级：{{classname}}</div>
+</template>
+
+<script>
+  import {inject} from 'vue'
+  export default{
+    setup(props, content) {
+      const student = inject('student')
+      return {...student}
+    },
+  }
+
+
+</script>
